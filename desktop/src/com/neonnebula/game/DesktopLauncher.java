07e4327021406da7ca4aea610a8e92b627d0d059
Jerkovic <erilundin@gmail.com>
2023-10-20 13:25:49 +0200

    initial commit

---
+++ desktop/src/com/neonnebula/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.neonnebula.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.neonnebula.game.NeonNebulaApplication;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("neon-nebula");
+		new Lwjgl3Application(new NeonNebulaApplication(), config);
+	}
+}
