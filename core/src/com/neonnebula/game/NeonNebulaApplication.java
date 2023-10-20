207aec1c7674f5439cedb1bf6e8fc3c7df6ccb70
Jerkovic <erilundin@gmail.com>
2023-10-20 14:20:51 +0200

    WIP

--- core/src/com/neonnebula/game/NeonNebulaApplication.java
+++ core/src/com/neonnebula/game/NeonNebulaApplication.java
@@ -3,6 +3,7 @@ package com.neonnebula.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FillViewport;
@@ -15,6 +16,8 @@ public class NeonNebulaApplication extends ApplicationAdapter {
 
     OrthographicCamera camera;
     Viewport viewport;
+
+    BitmapFont font;
 	
 	@Override
 	public void create () {
@@ -23,6 +26,9 @@ public class NeonNebulaApplication extends ApplicationAdapter {
 
         camera = new OrthographicCamera();
         viewport = new FitViewport(1024, 1024, camera);
+
+        font = new BitmapFont();
+        font.getData().setScale(2, 2);
 	}
 
     @Override
@@ -38,6 +44,8 @@ public class NeonNebulaApplication extends ApplicationAdapter {
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
+        batch.draw(img, 100, 100, 100, 100);
+        font.draw(batch, "Press any key", (camera.viewportWidth / 2f)-80, camera.viewportHeight / 2f);
 		batch.end();
 	}
 
